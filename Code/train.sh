2cfd399993b834531aa82b4336f8d5c34b25a535
Skylar Wurster <swwurster@gmail.com>
2021-04-15 11:59:34 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/ASMRSR
 
-python3 Code/main.py --save_name isomag2D_8.0sf --train_distributed True --num_workers 2 \
+python3 Code/main.py --save_name isomag2D --train_distributed True --num_workers 2 \
 --data_folder TrainingData/isomag2D --mode 2D --patch_size 1024 \
 --training_patch_size 48 --num_blocks 5 --base_num_kernels 64 \
 --x_resolution 1024 --y_resolution 1024 --epochs 1000 \
---scale_factor_end 32 --cropping_resolution 640
+--scale_factor_end 4 --cropping_resolution 48
