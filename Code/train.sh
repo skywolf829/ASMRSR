45a3cde5f1ee127ab9204059345d77d37a992cbc
Skylar Wurster <swwurster@gmail.com>
2021-04-25 18:12:17 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/ASMRSR
 
-python3 Code/main.py --save_name isomag2D_RDN5_64kernels_LIIFskip_512patch --train_distributed True --num_workers 8 \
+python3 Code/main.py --save_name isomag2D_RDN5_64kernels_LIIFskip_48patch --train_distributed True --num_workers 8 \
 --data_folder TrainingData/isomag2D --mode 2D --patch_size 1024 \
---training_patch_size 512 --cropping_resolution 512 \
+--training_patch_size 48 --cropping_resolution 48 \
 --feat_model RDN --upscale_model LIIF_skip \
 --num_blocks 5 --base_num_kernels 64 \
 --x_resolution 1024 --y_resolution 1024 --epochs 1000 \
